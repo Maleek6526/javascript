5363307cbaac5f51287ec88eb24ea2f679e39664
Adewale Maleek Ayobami <adewalemaleekayobami2528@gmail.com>
2024-10-17 11:27:13 +0100

    Task1.js

--- Phase Gate One/Task1.js
+++ Phase Gate One/Task1.js
@@ -1,10 +1,10 @@
-let userInput = 5;
+let userInput = 10;
 
 let originalUserInput = userInput;
 count = 1; 
 while(count < userInput){
 	originalUserInput *= userInput - count;
+	if(originalUserInput > 1000) break;
 	count++;
 	
-
 }console.log(originalUserInput);
