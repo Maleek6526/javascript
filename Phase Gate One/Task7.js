ccd6003645ed5f73407bb4b392b825e416349fc8
Adewale Maleek Ayobami <adewalemaleekayobami2528@gmail.com>
2024-10-17 15:24:46 +0100

    Task7.js

---
+++ Phase Gate One/Task7.js
@@ -0,0 +1,22 @@
+function getLeapYear(leapYear){
+
+
+	if(leapYear % 4 == 0 && leapYear % 100 != 0 || leapYear % 400 == 0) {
+
+		console.log("True")
+
+	}
+	
+	else{
+			console.log("False")
+
+		}
+
+
+}
+
+
+getLeapYear(2020);
+
+
+
