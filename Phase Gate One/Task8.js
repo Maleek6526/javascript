a5ac30da92e65bd42c7b34a13a0bf9fbdfafdc79
Adewale Maleek Ayobami <adewalemaleekayobami2528@gmail.com>
2024-10-17 15:24:58 +0100

    Task8.js

---
+++ Phase Gate One/Task8.js
@@ -0,0 +1,14 @@
+const studentGrade = {
+			math: 90,
+			english: 85,
+			science: 88
+			}
+
+
+for (const elements in studentGrade) {
+	console.log(`${elements}: ${studentGrade[elements]}`);
+
+}
+
+
+
