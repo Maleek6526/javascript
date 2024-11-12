1c82e287f552569e7037790f43b5d16aca1ec032
Adewale Maleek Ayobami <adewalemaleekayobami2528@gmail.com>
2024-11-12 10:35:16 +0100

    newfiletrial.js

--- newfiletrial.js
+++ newfiletrial.js
@@ -1,3 +1,4 @@
+/*
 let arrays = [[200,50,300,5], [10,25,7,100], [25,20,70,45], [500,170,11,35]];
 
 let arr = new Array(3);
@@ -8,3 +9,67 @@ for (let index = 0; index < arrays.length - 1; index++) {
     
 }
 console.log(arr);
+*/
+const objectArray = [[null, lagos = {
+    name : "Ade",
+    population : 1000,
+    temp : 10,
+    currency : "naira"
+}, abuja = {
+    name : "Wale",
+    population : 100000,
+    temp : 17,
+    currency : "dollars"
+},
+    akure = {
+        name : "Maleek",
+        population : 4000,
+        temp : 20,
+        currency : "pounds"
+    }], 
+    
+    [kano = {
+        name : "eda",
+        population : 3000,
+        temp : 11,
+        currency : "naira"
+    }, enugu = {
+        name : "Aed",
+        population : 1000,
+        temp : 10,
+        currency : "naira"
+    },
+    jigawa = {
+        name : "bawa",
+        population : 1000,
+        temp : 10,
+        currency : "naira"
+    },
+null], 
+    [kastina = {
+        name : "Ade",
+        population : 1000,
+        temp : 10,
+        currency : "naira"
+    },
+    
+    null,
+
+    benin = {
+        name : "Ade",
+        population : 1000,
+        temp : 10,
+        currency : "naira"
+    },
+
+    kwara = {
+        name : "Ade",
+        population : 1000,
+        temp : 10,
+        currency : "naira"
+    }
+
+]
+];
+
+console.log(objectArray[0][0]);
